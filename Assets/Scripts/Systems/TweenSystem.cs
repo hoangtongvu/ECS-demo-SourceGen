0e41357fdcb40ac0e856dd11fb83e27b58cf2c11
hoangtongvu <hoangtongvu@gmail.com>
2025-03-16 16:01:42 +0700

    TweenJob now is now IJC instead of IJE

--- Assets/Scripts/Systems/TweenSystem.cs
+++ Assets/Scripts/Systems/TweenSystem.cs
@@ -1,3 +1,6 @@
+using Core.Utilities.Extensions;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace TweenLib.Systems
@@ -6,9 +9,28 @@ namespace TweenLib.Systems
     [Unity.Burst.BurstCompile]
     public partial struct TransformPositionTweener_TweenSystem : ISystem
     {
+        private EntityQuery query;
+        private ComponentTypeHandle<Unity.Transforms.LocalTransform> componentTypeHandle;
+        private ComponentTypeHandle<Unity.Transforms.Can_TransformPositionTweener_TweenTag> canTweenTagTypeHandle;
+        private ComponentTypeHandle<Unity.Transforms.TransformPositionTweener_TweenData> tweenDataTypeHandle;
+
         [Unity.Burst.BurstCompile]
         public void OnCreate(ref Unity.Entities.SystemState state)
         {
+            EntityQueryBuilder queryBuilder = new EntityQueryBuilder(Allocator.Temp);
+
+            this.query = queryBuilder
+                .WithAllRW<Unity.Transforms.LocalTransform>()
+                .WithAllRW<Unity.Transforms.TransformPositionTweener_TweenData>()
+                .WithAll<Unity.Transforms.Can_TransformPositionTweener_TweenTag>()
+                .Build(ref state);
+
+            queryBuilder.Dispose();
+
+            this.componentTypeHandle = state.GetComponentTypeHandle<Unity.Transforms.LocalTransform>(false);
+            this.canTweenTagTypeHandle = state.GetComponentTypeHandle<Unity.Transforms.Can_TransformPositionTweener_TweenTag>(false);
+            this.tweenDataTypeHandle = state.GetComponentTypeHandle<Unity.Transforms.TransformPositionTweener_TweenData>(false);
+
             state.RequireForUpdate<Unity.Transforms.LocalTransform>();
             state.RequireForUpdate<Unity.Transforms.Can_TransformPositionTweener_TweenTag>();
             state.RequireForUpdate<Unity.Transforms.TransformPositionTweener_TweenData>();
@@ -17,38 +39,59 @@ namespace TweenLib.Systems
         [Unity.Burst.BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            new TweenJob
+            this.componentTypeHandle.Update(ref state);
+            this.canTweenTagTypeHandle.Update(ref state);
+            this.tweenDataTypeHandle.Update(ref state);
+
+            state.Dependency = new TweenIJC
             {
-                DeltaTime = SystemAPI.Time.DeltaTime,
-            }.ScheduleParallel();
+                DeltaTime = state.WorldUnmanaged.Time.DeltaTime,
+                ComponentTypeHandle = this.componentTypeHandle,
+                CanTweenTagTypeHandle = this.canTweenTagTypeHandle,
+                TweenDataTypeHandle = this.tweenDataTypeHandle,
+            }.ScheduleParallel(this.query, state.Dependency);
 
         }
 
         [Unity.Burst.BurstCompile]
-        public partial struct TweenJob : IJobEntity
+        public struct TweenIJC : IJobChunk
         {
             [Unity.Collections.ReadOnly] public float DeltaTime;
+            public ComponentTypeHandle<Unity.Transforms.LocalTransform> ComponentTypeHandle;
+            public ComponentTypeHandle<Unity.Transforms.Can_TransformPositionTweener_TweenTag> CanTweenTagTypeHandle;
+            public ComponentTypeHandle<Unity.Transforms.TransformPositionTweener_TweenData> TweenDataTypeHandle;
 
             [Unity.Burst.BurstCompile]
-            void Execute(
-                EnabledRefRW<Unity.Transforms.Can_TransformPositionTweener_TweenTag> canTweenTag
-                , ref Unity.Transforms.LocalTransform component
-                , ref Unity.Transforms.TransformPositionTweener_TweenData tweenData)
+            public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
             {
-                var tweener = new Utilities.TransformPositionTweener
-                {
-                    DeltaTime = this.DeltaTime,
-                };
+                var canTweenTagEnabledMask_RW = chunk.GetEnabledMask(ref this.CanTweenTagTypeHandle);
+                var componentArray = chunk.GetNativeArray(ref this.ComponentTypeHandle);
+                var tweenDataArray = chunk.GetNativeArray(ref this.TweenDataTypeHandle);
 
-                if (tweener.CanStop(in component, in tweenData.LifeTimeSecond, in tweenData.BaseSpeed, in tweenData.Target))
+                var enumerator = new ChunkEntityEnumerator(useEnabledMask, chunkEnabledMask, chunk.Count);
+
+                while (enumerator.NextEntityIndex(out var i))
                 {
-                    canTweenTag.ValueRW = false;
-                    tweenData.LifeTimeSecond = 0f;
-                    return;
+                    ref var component = ref componentArray.ElementAt(i);
+                    ref var tweenData = ref tweenDataArray.ElementAt(i);
+                    var canTweenTag = canTweenTagEnabledMask_RW.GetEnabledRefRW<Unity.Transforms.Can_TransformPositionTweener_TweenTag>(i);
+
+                    var tweener = new Utilities.TransformPositionTweener
+                    {
+                        DeltaTime = this.DeltaTime,
+                    };
+
+                    if (tweener.CanStop(in component, in tweenData.LifeTimeSecond, in tweenData.BaseSpeed, in tweenData.Target))
+                    {
+                        canTweenTag.ValueRW = false;
+                        tweenData.LifeTimeSecond = 0f;
+                        continue;
+                    }
+
+                    tweener.Tween(ref component, in tweenData.BaseSpeed, in tweenData.Target);
+                    tweenData.LifeTimeSecond += this.DeltaTime;
+
                 }
-
-                tweener.Tween(ref component, in tweenData.BaseSpeed, in tweenData.Target);
-                tweenData.LifeTimeSecond += this.DeltaTime;
 
             }
 
